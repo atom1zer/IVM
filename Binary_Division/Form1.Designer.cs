d0966638ddee70beee9bb7c7f193f62629deea64
atom1zer <dlsamsonov201133@gmail.com>
2021-08-26 21:51:35 +0300

    Добавьте файлы проекта.

---
+++ Binary_Division/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Binary_Division
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 49);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Разделить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = " А₁₀ :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "В₁₀ :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "А₂ :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 167);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "В₂ :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 213);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "С₁₀ :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(27, 249);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "С₂ :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 54);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(449, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Умножение  ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Binary_Division/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binary_Division
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		//Функция суммы в прямых кодах положительных чисел
+		static long Sum(long val1, long val2)
+		{
+			long i = 0, rem = 0, res = 0;
+			long[] sum = new long[40];
+
+			while (val1 != 0 || val2 != 0)
+			{
+				sum[i++] = (val1 % 10 + val2 % 10 + rem) % 2;
+				rem = (val1 % 10 + val2 % 10 + rem) / 2;
+				val1 = val1 / 10;
+				val2 = val2 / 10;
+			}
+			if (rem != 0)
+				sum[i++] = rem;
+
+			i = i - 1;
+
+			while (i >= 0)
+				res = res * 10 + sum[i--];
+			return res;
+		}
+
+		static int BinaryToDecimal(string binaryNumber)
+		{
+			int exponent = 0;
+			int result = 0;
+			for (var i = binaryNumber.Length - 1; i >= 0; i--)
+			{
+				if (binaryNumber[i] == '1')
+				{
+					result += Convert.ToInt32(Math.Pow(2, exponent));
+				}
+				exponent++;
+			}
+
+			return result;
+		}
+		static string Dop_Kod(string num, string s)
+		{
+			int minus = s.IndexOf('-');
+			if (minus == 0)
+			{
+				string dopkod = "";
+				dopkod = Convert.ToString(Sum(Convert.ToInt64(Obratniy_Kod(num, s)), 1));
+				return dopkod;
+			}
+			else
+				return num;
+		}
+		static string Obratniy_Kod(string num1, string n)
+		{
+			int minus = n.IndexOf('-');
+			string obkod = "";
+			if (minus == 0)
+			{
+				char[] a = num1.ToCharArray();
+				int[] b = new int[a.Length];
+				for (int i = 1; i < a.Length; i++)
+				{
+					if (a[i] == '0')
+					{
+						b[i] = 1;
+					}
+					if (a[i] == '1')
+					{
+						b[i] = 0;
+					}
+					obkod += Convert.ToString(b[i]);
+				}
+				return obkod;
+			}
+			else
+			{
+				return num1;
+			}
+
+		}
+
+		static string Division(int n1, int n2, char[] a, char[] b)
+		{
+			string res;
+			string ca, cb, n_iter, n_iter2;
+			int len = n1 / n2;
+			string slen = Convert.ToString(len, 2);
+			
+			char[] l = slen.ToCharArray();
+			char[] result = new char[l.Length];
+			ca = string.Join("", a);
+			cb = string.Join("", b);
+			string dop_b = Dop_Kod(cb, "-");
+			if (a.Length >= b.Length)
+			{
+				char[] c = new char[b.Length];
+				for (int j = 0; j <= b.Length - 1; j++)
+				{
+					c[j] = a[j];
+				}
+				ca = string.Join("", c);
+				cb = string.Join("", b);
+				string obr_cb = Dop_Kod(cb, "-");
+				string res1 = Convert.ToString(Sum(Convert.ToInt64(ca), Convert.ToInt64(obr_cb)));
+				n_iter = res1.Substring(1, res1.Length - 1);
+				
+				result[0] = '1';
+				for (int i = 1; i <= l.Length-1; i++)
+				{
+					n_iter2 = n_iter + a[(b.Length-1) + i];
+					
+					int btd2 = BinaryToDecimal(n_iter2);
+					MessageBox.Show(n_iter2);
+					if (btd2 > n2)
+					{
+						result[i] = '1';
+						string res2 = Convert.ToString(Sum(Convert.ToInt64(n_iter2), Convert.ToInt64(obr_cb)));
+						//MessageBox.Show(res2);
+					}
+					else
+					{
+						result[i] = '0';
+						n_iter2 = n_iter2 + a[(b.Length-1) + (i+1)];
+						MessageBox.Show("fdjksb" + n_iter2);
+					}
+				}
+				res = string.Join("", result);
+			}
+			else
+			{
+				char[] c = new char[a.Length];
+				for (int j = 0; j <= a.Length - 1; j++)
+				{
+					c[j] = a[j];
+				}
+				res = string.Join("", c);
+			}
+			
+			return res;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+
+            string ss = "";
+            //Перевод в коды
+            int minus = textBox1.Text.IndexOf('-');
+            string text = textBox1.Text.Trim(new char[] { '-' });
+            int k = Convert.ToInt32(text);
+            string dig = Convert.ToString(k, 2);
+            if (minus == 0)
+            {
+                for (int i = 1; i < 16 - (dig.Length - 1); i++)
+                {
+                    ss += "0";
+                }
+                label1.Text = "";
+                label1.Text = dig;
+
+            }
+            else
+            {
+                for (int i = 1; i < 16 - (dig.Length - 1); i++)
+                {
+                    ss += "0";
+                }
+                label1.Text = "";
+                label1.Text = dig;
+            }
+            string ss2 = "";
+            int minus2 = textBox2.Text.IndexOf('-');
+            string text2 = textBox2.Text.Trim(new char[] { '-' });
+            int m = Convert.ToInt32(text2);
+            string dig2 = Convert.ToString(m, 2);
+            if (minus2 == 0)
+            {
+
+                for (int i = 1; i < 16 - (dig2.Length - 1); i++)
+                {
+                    ss2 += "0";
+                }
+                label2.Text = "";
+                label2.Text = dig2;
+            }
+            else
+            {
+                for (int i = 1; i < 16 - (dig2.Length - 1); i++)
+                {
+                    ss2 += "0";
+                }
+                label2.Text = "";
+                label2.Text = dig2;
+            }
+
+            int n1 = Convert.ToInt32(textBox1.Text);
+			int n2 = Convert.ToInt32(textBox2.Text);
+			string otv =Convert.ToString(division1(n1, n2));
+			//string otv_res = Convert.ToString(Convert.ToInt32(otv), 2);
+			
+			int minus1 = textBox1.Text.IndexOf('-');
+			minus2 = textBox2.Text.IndexOf('-');
+			if ((minus1 == 0 && minus2 !=0) || (minus1 != 0 && minus2 == 0))
+			{
+				label3.Text = otv;
+				string n_otv = otv.Remove(0, 1);
+				string otv_res = Convert.ToString(Convert.ToInt32(n_otv), 2);
+				label4.Text = "1. " + otv_res;
+			}
+			else if ((minus1 == 0 && minus2 == 0) || (minus1 != 0 && minus2 != 0))
+			{
+				label3.Text = otv;
+				string otv_res = Convert.ToString(Convert.ToInt32(otv), 2);
+				label4.Text = otv_res;
+			}
+
+
+           
+
+        }
+
+		int remainder = 0;
+		int division1(int dividend, int divisor)
+		{
+			int quotient = 1;
+
+			int neg = 1;
+			if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+				neg = -1;
+
+			// Convert to positive
+			int tempdividend = (dividend < 0) ? -dividend : dividend;
+			int tempdivisor = (divisor < 0) ? -divisor : divisor;
+
+			if (tempdivisor == tempdividend)
+			{
+				remainder = 0;
+				return 1 * neg;
+			}
+			else if (tempdividend < tempdivisor)
+			{
+				if (dividend < 0)
+					remainder = tempdividend * neg;
+				else
+					remainder = tempdividend;
+				return 0;
+			}
+			while (tempdivisor << 1 <= tempdividend)
+{
+				tempdivisor = tempdivisor << 1;
+				quotient = quotient << 1;
+			}
+
+			// Call division recursively
+			if (dividend < 0)
+				quotient = quotient * neg + division1(-(tempdividend - tempdivisor), divisor);
+			else
+				quotient = quotient * neg + division1(tempdividend - tempdivisor, divisor);
+			return quotient;
+		}
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label1.Text = " ";
+            label2.Text = " ";
+            label3.Text = " ";
+            label4.Text = " ";
+
+        }
+    }
+}
